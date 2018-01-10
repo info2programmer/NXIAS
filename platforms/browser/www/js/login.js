9bb368abbb65f7c484ee0b5228d208573797783a
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-01-10 14:55:40 +0530

    exit app

--- platforms/browser/www/js/login.js
+++ platforms/browser/www/js/login.js
@@ -33,7 +33,21 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        
+         document.addEventListener("backbutton", onBackKeyDown, false); 
+		 
+		 function onBackKeyDown(e) {
+			e.preventDefault();
+			navigator.notification.confirm("Are you sure you want to exit ?", onConfirm, "Confirmation", "Yes,No"); 
+			// Prompt the user with the choice
+		}
+
+		function onConfirm(button) {
+			if(button==2){//If User selected No, then we just do nothing
+				return;
+			}else{
+				navigator.app.exitApp();// Otherwise we quit the app.
+			}
+		}
 
         $('#btnLogin').click(function () {
             if ($('#txtUsername').val() == "" || $('#txtPassword').val() ==""){
