e504fe6901d447a180f9d5b167c3f23c037a6eb0
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-01-10 18:19:18 +0530

    Registration Page Internet Connection Check

--- www/js/registration.js
+++ www/js/registration.js
@@ -27,13 +27,14 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener("offline", checkConnection, false);
+        
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        document.addEventListener("offline", checkConnection, false);
         // app.receivedEvent('deviceready');
 		$('#btnReg').click(function () { 
 									 
