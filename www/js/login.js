9acd1e446de538724055f1feba4e5826b92db92e
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-01-10 15:10:03 +0530

    login exit

--- www/js/login.js
+++ www/js/login.js
@@ -48,28 +48,29 @@ var app = {
 //			}
 //		}
 
+document.addEventListener('deviceready', function() {
+    var exitApp = false, intval = setInterval(function (){exitApp = false;}, 1000);
+    document.addEventListener("backbutton", function (e){
+        e.preventDefault();
+        if (exitApp) {
+            clearInterval(intval) 
+			navigator.notification.confirm("Are you sure you want to exit ?", onConfirm, "Confirmation", "Yes,No"); 
+            (navigator.app && navigator.app.exitApp()) || (device && device.exitApp())
+        }
+        else {
+            exitApp = true
+            history.back(1);
+        } 
+    }, false);
+}, false);
 
-		var lastTimeBackPress=0;
-var timePeriodToExit=2000;
-
-function onBackKeyDown(e){
-    e.preventDefault();
-    e.stopPropagation();
-    if(new Date().getTime() - lastTimeBackPress < timePeriodToExit){
-        navigator.app.exitApp();
-    }else{
-        window.plugins.toast.showWithOptions(
-            {
-              message: "Press again to exit.",
-              duration: "short", // which is 2000 ms. "long" is 4000. Or specify the nr of ms yourself.
-              position: "bottom",
-              addPixelsY: -40  // added a negative value to move it up a bit (default 0)
-            }
-          );
-        
-        lastTimeBackPress=new Date().getTime();
-    }
-};
+			function onConfirm(button) {
+						if(button==2){//If User selected No, then we just do nothing
+							return;
+					}else{
+							navigator.app.exitApp();// Otherwise we quit the app.
+						}
+					}
 
         $('#btnLogin').click(function () {
             if ($('#txtUsername').val() == "" || $('#txtPassword').val() ==""){
