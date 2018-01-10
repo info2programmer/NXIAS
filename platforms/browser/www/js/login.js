c66df94474612a035c1abd82b304b44c388c404e
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-01-10 15:13:01 +0530

    Testing app exit

--- platforms/browser/www/js/login.js
+++ platforms/browser/www/js/login.js
@@ -27,7 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener("backbutton", onBackKeyDown, false); 
+        //document.addEventListener("backbutton", onBackKeyDown, false); 
     },
     // deviceready Event Handler
     //
